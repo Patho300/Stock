f2372c5b54e68e88110ac828562445e8db0bf738
patricio <pathoh2@gmail.com>
2019-12-05 15:01:19 -0300

    Se agregan los directorios de trabajo, se modifica la validación del login (no-encriptado)

---
+++ dash.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+echo '<h1>Esto es uan prueba que resulto exitosa!</h1>';
+
+
+?>
+
---
+++ includes/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+function mostrarError ($errores, $campo){
+
+$alerta = '';
+if(isset($errores[$campo]) && !empty($campo)){
+   $alerta ="<div class='alerta alerta-error'>".errores[$campo].'</div>';
+
+}
+return $alerta;
+
+}
+
+
+?>
